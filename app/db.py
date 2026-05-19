7b8fb22231676a79d1a90ff4a9c20cf13d3f4bf9
Gianluca-gitea <44906021@alumnos.frsr.utn.edu.ar>
2026-05-19 20:12:57 -0300

    DB creds

--- app/db.py
+++ app/db.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timezone
 from pymongo import MongoClient
-import time
 
-mongo_uri = "mongodb://Usuario_bd:NcwXuCDKBmIWBrSv@ac-2modq9p-shard-00-00.fhhfn2f.mongodb.net:27017,ac-2modq9p-shard-00-01.fhhfn2f.mongodb.net:27017,ac-2modq9p-shard-00-02.fhhfn2f.mongodb.net:27017/?ssl=true&replicaSet=atlas-6mj7et-shard-0&authSource=admin&appName=pdf-extractext"
+from app.settings import get_settings
 
+settings = get_settings()
+mongo_client = MongoClient(settings.mongo_uri)
 
 #para guardar en la base de datos, el formato es el siguiente:
 {
