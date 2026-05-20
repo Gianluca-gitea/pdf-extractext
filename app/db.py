2ee3130d6a037f38a42fcdbbd27d36d4809cd8f4
Gianluca-gitea <44906021@alumnos.utn.frsr.edu.ar>
2026-05-19 23:46:46 -0300

    Fixes de lint, Flake8 viene gritando hace rato

--- app/db.py
+++ app/db.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime
 from pymongo import MongoClient
 
 from app.settings import get_settings
@@ -6,13 +6,13 @@ from app.settings import get_settings
 settings = get_settings()
 mongo_client = MongoClient(settings.mongo_uri)
 
-#para guardar en la base de datos, el formato es el siguiente:
-{
-    "pdf_nombre": str,  #El nombre del archivo PDF
-    "txt_contenido": str,   #El contenido del archivo txt
-    "txt_chars": int,   #La cantidad de caracteres del contenido del txt
-    "estado": "ok" | "error",   #El estado de procesamiento del PDF, solo puede ser "ok" o "error"
-    "error": str | None,    #Si el proceso devuelve error, aqui muestra el error, de lo contrario devuelve none
-    "created_at": datetime, #La fecha y hora en que se creó el registro
-    "duracion_ms": int  #La duración del proceso en ms
-}
+# para guardar en la base de datos, el formato es el siguiente:
+# {
+#     "pdf_nombre": str,  # El nombre del archivo PDF
+#     "txt_contenido": str,  # El contenido del archivo txt
+#     "txt_chars": int,  # La cantidad de caracteres del contenido del txt
+#     "estado": "ok" | "error",  # El estado de procesamiento del PDF, solo puede ser "ok" o "error"
+#     "error": str | None,  # Si el proceso devuelve error, aqui muestra el error, de lo contrario devuelve none
+#     "created_at": datetime,  # La fecha y hora en que se creó el registro
+#     "duracion_ms": int,  # La duración del proceso en ms
+# }
