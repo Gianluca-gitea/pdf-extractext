d620bb70b5255d054e65c26d0f63db78b61415a4
Gianluca-gitea <44906021@alumnos.frsr.utn.edu.ar>
2026-05-23 02:22:33 -0300

    despues testeo el de windows

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ echo "========================================="
 echo "  Iniciando PDF Extractext"
 echo "========================================="
 
+source venv/bin/activate
+
 echo "[INFO] Asegurate de que MongoDB esté corriendo en el puerto 27017..."
 
 echo "[INFO] Iniciando el backend..."
@@ -18,4 +20,5 @@ python app/interface.py
 
 echo "[INFO] Interfaz cerrada. Deteniendo el backend..."
 kill $BACKEND_PID
+wait $BACKEND_PID 2>/dev/null
 echo "[INFO] Listo."
